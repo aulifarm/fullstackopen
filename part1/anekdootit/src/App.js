42ed39fc10a4fb8038f94c0e480f8ddbba3f849c
Auli Färm <farmauli@gmail.com>
2021-04-19 15:32:14 +0300

    Submit exercises 1.12-1.14

--- part1/anekdootit/src/App.js
+++ part1/anekdootit/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState([0,0,0,0,0,0])
+ 
+
+  const handleVoteClick = () => {
+    const votesArray = [...votes]
+    votesArray[selected] += 1
+    setVotes(votesArray)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]} 
+      <br/>
+      <br/>
+      This anecdote has {votes[selected]} votes</p>
+      <button onClick={() => setSelected(Math.floor(Math.random() * 6))} >Next anecdote</button>
+      <button onClick={handleVoteClick}>Vote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
